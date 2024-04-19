0861c76be4ad96afde2e57ad1413ed679ebd79b8
RHappiness <a2a45a789@gmail.com>
2024-04-19 23:46:59 +0800

    2024
    2024/4/19

--- adding_team.js
+++ adding_team.js
@@ -1,6 +1,9 @@
    function addTeammate() {
         const nameInput = document.getElementById("teammateName");
         const name = nameInput.value;
+        shiftd=document.getElementById("inputX").value;
+        shiftu=document.getElementById("inputY").value;
+        let shiftnow=0;
         if (!name) {
             alert("請輸入隊友名稱");
             return;
@@ -13,7 +16,7 @@
             table.rows[row].cells[col].style.backgroundColor = "white";
         });
         currentSelectedCells = [];
-        teammates.push({ name, blocked });
+        teammates.push({ name, blocked ,shiftd,shiftu,shiftnow});
         console.log(teammates);
       //  console.log(typeof teammates[0].blocked[0]);
         // Clear teammate name input
